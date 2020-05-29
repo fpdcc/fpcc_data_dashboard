c85f216039ce718de97ed5a08955ba0c6c711f0a
glw <garretlewis@gmail.com>
2020-05-28 21:15:32 -0500

    start dashapp2

--- app/dashapp2/callbacks.py
+++ app/dashapp2/callbacks.py
@@ -1,15 +1,15 @@
-from datetime import datetime as dt
-
 from dash.dependencies import Input
 from dash.dependencies import Output
-
+from .layout import *
 
 def register_callbacks(dashapp):
-    @dashapp.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
-    def update_graph(selected_dropdown_value):
-        return dict({
-        "data": [{"type": "bar",
-              "x": [1, 2, 3],
-              "y": [1, 3, 2]}],
-            'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
-            })
+    @dashapp.callback(Output("content", "children"), [Input("tabs", "active_tab")])
+    def switch_tab(at):
+        if at == "home":
+            return "home content"
+
+        elif at == "general":
+            return "general content"
+
+        elif at == "details":
+            return "details content"
--- app/dashapp2/layout.py
+++ app/dashapp2/layout.py
@@ -1,16 +1,70 @@
+# Imports
+import sys, os
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_table
+import dash_table.FormatTemplate as FormatTemplate
+import pandas as pd
+import pandas.io.sql as psql
+import plotly.express as px
+import psycopg2 as pg
+import numpy as np
+import json
+import geojson
+from pathlib import Path
 
-layout = html.Div([
-    html.H1('Stock Tickers'),
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'Coke', 'value': 'COKE'},
-            {'label': 'Tesla', 'value': 'TSLA'},
-            {'label': 'Apple', 'value': 'AAPL'}
-        ],
-        value='COKE'
-    ),
-    dcc.Graph(id='my-graph')
-], style={'width': '500'})
+basedir = Path(os.path.abspath(os.path.dirname(__file__))).parent
+datadir = Path(basedir / 'assets')
+POSTGRESQL = os.environ.get('POSTGRESQL')
+
+#project data
+conn_string=POSTGRESQL
+connection=pg.connect(conn_string)
+cur = connection.cursor()
+
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Buildings", href="#")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("Data Sets", header=True),
+                dbc.DropdownMenuItem("Trails", href="#"),
+                dbc.DropdownMenuItem("Parking Lots", href="#"),
+                dbc.DropdownMenuItem("Picnic Groves", href="#"),
+                dbc.DropdownMenuItem("CIP", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="More",
+        ),
+    ],
+    brand="FPCC Data",
+    brand_href="#",
+    color="#015249",
+    dark=True,
+)
+
+body = dbc.Container(
+    children=[
+        html.Div([
+        dbc.Tabs(
+            [
+                dbc.Tab(label="Home", tab_id="home"),
+                dbc.Tab(label="General", tab_id="general"),
+                dbc.Tab(label="Details", tab_id="details"),
+            ],
+            id="tabs",
+            active_tab="home",
+            className="nav nav-pills nav-fill mt-3",
+        ),
+        html.Div(id="content"),
+        ]),
+
+
+
+    ],fluid=True)
+
+layout = html.Div([navbar, body])
